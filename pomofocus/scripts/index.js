ea35b4754d9d3e4934552c9f3c1bf3e452c48a81
Luiz Fernando de Oliveira Alves <lfoalves.dev@gmail.com>
2022-08-13 22:35:13 -0400

    :sparkles:

--- pomofocus/scripts/index.js
+++ pomofocus/scripts/index.js
@@ -1,67 +1,44 @@
 // EcmaScript6 - 2015 ES6 Module
-import resetControls from './controls.js'; // import default porde ser qualquer nome
-import { Timer } from './timer.js' // named export aceita somente os nomes exportados
+import Controls from './controls.js'; // import default porde ser qualquer nome
+import Timer from './timer.js' // named export aceita somente os nomes exportados
+import {
+  buttonPlay,
+  buttonPause,
+  buttonSet,
+  buttonStop,
+  buttonSoundOn,
+  buttonSoundOff,
+  minutesDisplay,
+  secondsDisplay
+} from './elements.js';
+import Sound from './sounds.js';
+import Events from './events.js';
 
-const buttonPlay = document.querySelector('.play');
-const buttonPause = document.querySelector('.pause');
-const buttonSet = document.querySelector('.set');
-const buttonStop = document.querySelector('.stop');
+// Injeção de dependências
+const controls = Controls({
+  buttonPlay,
+  buttonPause,
+  buttonSet,
+  buttonStop
+});
 
-const buttonSoundOn = document.querySelector('.sound-on');
-const buttonSoundOff = document.querySelector('.sound-off');
-
-let minutesDisplay = document.querySelector('.minutes');
-let secondsDisplay = document.querySelector('.seconds');
-
-let minutes = Number(minutesDisplay.textContent);
-let seconds;
-let timertimeOut;
-
-
+// Injeção de dependências
 const timer = Timer({
   minutesDisplay,
   secondsDisplay,
-  timertimeOut,
-  resetControls
+  resetControls: controls.reset
 });
 
+const sound = Sound();
 
-buttonPlay.addEventListener('click', function() {
-  buttonPlay.classList.add('hide');
-  buttonPause.classList.remove('hide');
-  buttonSet.classList.add('hide');
-  buttonStop.classList.remove('hide');
-
-  timer.countdown();    
-
-});
-buttonPause.addEventListener('click', function() {
-  buttonPause.classList.add('hide');
-  buttonPlay.classList.remove('hide');
-
-  clearTimeout(timertimeOut);
-});
-buttonStop.addEventListener('click', function() {
-  resetControls();
-  timer.resetTimer();
-});
-buttonSet.addEventListener('click', function() {
-  let newMinutes = parseInt(Number(prompt('Quantos minutos')));
-
-  if (!newMinutes) {
-    resetTimer();
-    return;
-  }
-  
-  minutes = newMinutes;
-  updateTimerDisplay(minutes, 0);
-});
-
-buttonSoundOn.addEventListener('click', function() {
-  buttonSoundOn.classList.add('hide');
-  buttonSoundOff.classList.remove('hide');
-});
-buttonSoundOff.addEventListener('click', function() {
-  buttonSoundOff.classList.add('hide');
-  buttonSoundOn.classList.remove('hide');
+Events({
+  buttonPlay,
+  buttonPause,
+  buttonSet,
+  buttonStop,
+  buttonSoundOn,
+  buttonSoundOff,
+  controls,
+  timer,
+  sound
 });
